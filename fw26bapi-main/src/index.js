af2cc6167dd1904427d0bfb24a1b4425845d4fe9
Tonin <152934527+antoninho067@users.noreply.github.com>
2025-11-28 08:32:12 -0300

    Add files via upload

---
+++ fw26bapi-main/src/index.js
@@ -0,0 +1,15 @@
+// Use "type: commonjs" in package.json to use CommonJS modules
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs')
+app.set('views',"./views")
+
+require("../db/db")(app);
+require("../rotas/home")(app);
+require("../rotas/getnoticias")(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
